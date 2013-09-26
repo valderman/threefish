601cfc30aa14706b16b7f9cb8393d1b1e3469793
Anton Ekblad <anton@ekblad.cc>
2013-09-26 21:48:01 +0200

    Split c_hash256 into smaller parts to prepare for lazy bytestrings.

--- cbits/skein256.c
+++ cbits/skein256.c
@@ -1,45 +1,48 @@
 #include "threefish.h"
 #include <string.h>
 
-void hash256(W64* key, W64* nonce, W64 len, W64* data, int outlen, W64* out) {
-  W64 config[4] = {0x0000000133414853,
-                   /* size parameter is 2^64-1 for stream cipher mode */
-                   nonce ? 0xffffffffffffffffL : outlen*8,
-                   0, 0};
-  W64 tweak[2];
-  W64 buf[4];
-  W64 k[4] = {0,0,0,0};
-  W64 lastlen = len % 32;
-  if(lastlen == 0 && len != 0) {
-    lastlen = 32;
-  }
+void skein256_init(skein_t* ctx, W64* key, int outlen) {
+  W64 config[4] = {0x0000000133414853, outlen*8, 0, 0};
+  static W64 zeroes[4] = {0,0,0,0};
 
   /* Set up key if needed */
   if(key != NULL) {
-    init_tweak(T_KEY, tweak);
-    set_last(1, tweak);
-    add_bytes(32, tweak);
-    encrypt256(k, tweak[0], tweak[1], key, k);
-    k[0] ^= key[0]; k[1] ^= key[1]; k[2] ^= key[2]; k[3] ^= key[3];
+    init_tweak(T_KEY, ctx->tweak);
+    set_last(1, ctx->tweak);
+    add_bytes(32, ctx->tweak);
+    encrypt256(zeroes, ctx->tweak[0], ctx->tweak[1], key, ctx->key);
+    ctx->key[0] ^= key[0];
+    ctx->key[1] ^= key[1];
+    ctx->key[2] ^= key[2];
+    ctx->key[3] ^= key[3];
+  } else {
+    ctx->key[0] = 0; ctx->key[1] = 0; ctx->key[2] = 0; ctx->key[3] = 0;
   }
 
   /* Process config string */
-  mk_config_tweak(tweak);
-  add_bytes(32, tweak);
-  encrypt256(k, tweak[0], tweak[1], config, k);
-  k[0] ^= config[0]; k[1] ^= config[1]; k[2] ^= config[2]; k[3] ^= config[3];
+  mk_config_tweak(ctx->tweak);
+  add_bytes(32, ctx->tweak);
+  encrypt256(ctx->key, ctx->tweak[0], ctx->tweak[1], config, ctx->key);
+  ctx->key[0] ^= config[0];
+  ctx->key[1] ^= config[1];
+  ctx->key[2] ^= config[2];
+  ctx->key[3] ^= config[3];
+}
 
-  /* Process nonce, if available; must be 32 bytes */
-  if(nonce != NULL) {
-    init_tweak(T_NONCE, tweak);
-    set_last(1, tweak);
-    add_bytes(32, tweak);
-    encrypt256(k, tweak[0], tweak[1], nonce, k);
-    k[0] ^= nonce[0]; k[1] ^= nonce[1]; k[2] ^= nonce[2]; k[3] ^= nonce[3];
-  }
+void skein256_update(skein_t* ctx, int firstlast, UBIType type, W64 len, W64* data) {
+  W64 buf[4] = {0,0,0,0};
+  W64* k = ctx->key;
+  W64* tweak = ctx->tweak;
+  int lastlen;
 
   /* Process message */
-  init_tweak(T_MSG, tweak);
+  if(firstlast & 1) {
+    init_tweak(type, tweak);
+  }
+  /* If this is not the last update, don't do last block processing  */
+  if(!(firstlast & 2) && len % 32 == 0) {
+    ++len;
+  }
   while(len > 32) {
     add_bytes(32, tweak);
     encrypt256(k, tweak[0], tweak[1], data, k);
@@ -52,25 +55,51 @@ void hash256(W64* key, W64* nonce, W64 len, W64* data, int outlen, W64* out) {
   }
 
   /* Process last block */
-  add_bytes(lastlen, tweak);
-  set_last(1, tweak);
-  buf[0] = buf[1] = buf[2] = buf[3] = 0;
-  memcpy(buf, data, lastlen);
-  encrypt256(k, tweak[0], tweak[1], buf, k);
-  k[0] ^= buf[0];
-  k[1] ^= buf[1];
-  k[2] ^= buf[2];
-  k[3] ^= buf[3];
+  if(firstlast & 2) {
+    lastlen = len % 32;
+    if(lastlen == 0 && len > 0) {
+      lastlen = 32;
+    }
+    add_bytes(lastlen, tweak);
+    set_last(1, tweak);
+    memcpy(buf, data, lastlen);
+    encrypt256(k, tweak[0], tweak[1], buf, k);
+    k[0] ^= buf[0];
+    k[1] ^= buf[1];
+    k[2] ^= buf[2];
+    k[3] ^= buf[3];
+  }
+}
 
-  /* Output pass */
-  buf[0] = 0; buf[1] = 0; buf[2] = 0; buf[3] = 0;
-  while(outlen > 0) {
+void skein256_output(skein_t* ctx, int from, int to, W64* out) {
+  W64 buf[4] = {from,0,0,0};
+  W64 *k = ctx->key;
+  W64 *tweak = ctx->tweak;
+  for(; from <= to; ++from) {
     init_tweak(T_OUT, tweak);
     set_last(1, tweak);
     add_bytes(8, tweak);
     encrypt256(k, tweak[0], tweak[1], buf, out);
-    outlen -= 32;
     out += 4;
     ++buf[0];
   }
+}
+
+void hash256(W64* key, W64* nonce, W64 len, W64* data, int outlen, W64* out) {
+  skein_t ctx;
+
+  skein256_init(&ctx, key, outlen);
+
+  /* Process nonce, if available; must be 32 bytes */
+  if(nonce != NULL) {
+    skein256_update(&ctx, 3, T_NONCE, 32, nonce);
+  }
+
+  skein256_update(&ctx, 3, T_MSG, len, data);
+  if(outlen % 32 == 0) {
+    outlen = outlen / 32;
+  } else {
+    outlen = (outlen / 32) + 1;
+  }
+  skein256_output(&ctx, 0, outlen, out);
 }
--- cbits/threefish.h
+++ cbits/threefish.h
@@ -3,6 +3,13 @@
 
 typedef unsigned long long W64;
 extern W64 key_const;
+
+typedef struct {
+  W64 key[4];
+  W64 tweak[2];
+  int outlen;
+} skein_t;
+
 #define rl(x, b) (((x) << ((b) & 63)) | ((x) >> ((64-(b)) & 63)))
 #define rr(x, b) (((x) >> ((b) & 63)) | ((x) << ((64-(b)) & 63)))
 
@@ -19,6 +26,11 @@ typedef enum {
 
 void encrypt256(W64* key, W64 t0, W64 t1, W64* in, W64* out);
 void decrypt256(W64* key, W64 t0, W64 t1, W64* in, W64* out);
+
+void skein256_init(skein_t* ctx, W64* key, int outlen);
+/* firstlast & 1 if we're starting a new type, firstlast & 2 if it's the last update */
+void skein256_update(skein_t* ctx, int firstlast, UBIType type, W64 len, W64* data);
+void skein256_output(skein_t* ctx, int from, int to, W64* out);
 
 /* IMPORTANT: out must point to memory enough to hold outblock*32 bytes! */
 void hash256(W64* key,
